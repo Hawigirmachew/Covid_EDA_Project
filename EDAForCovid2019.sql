8c05973b77a1d430d669914b00522195e63ebd4e
Hawi Girmachew <88828065+Hawigirmachew@users.noreply.github.com>
2024-02-02 00:05:06 +0300

    Adding new Temp tables in EDAForConvid2019

--- EDAForCovid2019.sql
+++ EDAForCovid2019.sql
@@ -1,23 +1,3 @@
-select * 
-from dbo.CovidDeaths$
---WHERE location = 'Ethiopia'
-ORDER BY 1,2
-
---total cases vs total death
-SELECT  location, population, (total_deaths/total_cases) * 100 as DeathPercentage
-from dbo.CovidDeaths$
-
---total cases vs population
-SELECT location, population, total_cases, (total_cases / population ) * 100 as InfectedPop
-FROM dbo.CovidDeaths$
-order by InfectedPop desc
-
---countries with highest infection rate compared to population
-SELECT continent,  MAX(CAST(total_cases as int)) as TotalDeaths,MAX(CAST(total_cases as int) / population ) * 100 as InfectedPop
-FROM dbo.CovidDeaths$
-WHERE continent is not null
-GROUP BY continent
-order by InfectedPop desc
 
 select * 
 from dbo.CovidDeaths$
@@ -105,6 +85,71 @@ where cd.continent is not null and cv.new_vaccinations is not null
 Select *, (PopVaccinated/Population) * 100 as PercentageOfPopVaccinated
 from #PercentagePopVaccinated
 --order by 3
+
+
+DROP table if exists #HighestInfectionRateCountries
+Create Table #HighestInfectionRateCountries
+(
+continent nvarchar(255),
+TotalDeaths numeric,
+InfectedPop numeric,
+)
+
+Insert into #HighestInfectionRateCountries
+SELECT continent,  MAX(CAST(total_cases as int)) as TotalDeaths,MAX(CAST(total_cases as int) / population ) * 100 as InfectedPop
+FROM [Portfolio Project].[dbo].[CovidDeaths$]
+WHERE continent is not null
+GROUP BY continent
+
+DROP table if exists #TotalNewCasesTotalNewDeathsPerDay 
+Create Table #TotalNewCasesTotalNewDeathsPerDay 
+(
+Date datetime,
+ Total_new_cases numeric,
+Total_new_deaths numeric,
+DeathPercentage numeric
+
+)
+
+Insert into #TotalNewCasesTotalNewDeathsPerDay 
+SELECT  date, SUM(new_cases) as Total_new_cases, SUM(CAST(new_deaths as int)) as Total_new_deaths, ( SUM(CAST(new_deaths as int))/SUM(new_cases)) * 100 as DeathPercentage
+from [Portfolio Project].[dbo].[CovidDeaths$]
+where continent is not null
+group by date
+
+
+DROP table if exists #TotalCasesVsTotalDeaths
+Create Table #TotalCasesVsTotalDeaths 
+(
+Location nvarchar(255),
+Population numeric,
+DeathPercentage numeric
+
+)
+
+Insert into #TotalCasesVsTotalDeaths
+SELECT  location, population, (total_deaths/total_cases) * 100 as DeathPercentage
+from [Portfolio Project].[dbo].[CovidDeaths$]
+
+
+
+
+DROP table if exists #TotalCasesVSPopulationPerLocation
+Create Table #TotalCasesVSPopulationPerLocation 
+(
+Location nvarchar(255),
+TotalPopulation numeric,
+TotalCases numeric,
+InfectedPop numeric
+
+)
+Insert into #TotalCasesVSPopulationPerLocation 
+SELECT location, Sum(population) as TotalPopulation, Sum(total_cases) as TotalCases, (Sum(total_cases) / SUM(population) ) * 100 as InfectedPop
+FROM [Portfolio Project].[dbo].[CovidDeaths$]
+GROUP BY location 
+HAVING sum(total_cases) >= 20000
+order by InfectedPop desc
+
 
 
 --view
